05b81d0ec05b16fae7674a391a5cf025d5454dd4
LaithDribaty <69043539+LaithDribaty@users.noreply.github.com>
2021-01-07 17:28:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var app = new Vue({
     el: "#app",
     data: {
         messages: [
-            {val: "what's up bro" , speaker: true} , 
-            {val: "what's up bro" , speaker: false} ,
+            {val: "hello" , speaker: true} , 
+            {val: "Hi , how are you?" , speaker: false} ,
         ],
         numOfKeyboard1: 1 ,
         numOfKeyboard2: 1 ,        
@@ -25,13 +25,13 @@ var app = new Vue({
             var id = parseInt(e.target.id);
             this.numOfKeyboard1 = parseInt(e.target.value);
             var box = document.getElementById("box"+id);
-            box.style.height = ( 600 + (this.numOfKeyboard1 - 1)*60 )+"px";
+            box.style.height = ( 600 + (this.numOfKeyboard1 - 1)*70 )+"px";
         },
         changeKeyboards2( e ){
             var id = parseInt(e.target.id);
             this.numOfKeyboard2 = parseInt(e.target.value);
             var box = document.getElementById("box"+id);
-            box.style.height = ( 600 + (this.numOfKeyboard2 - 1)*60 )+"px";
+            box.style.height = ( 600 + (this.numOfKeyboard2 - 1)*70 )+"px";
         }
     }
 });
